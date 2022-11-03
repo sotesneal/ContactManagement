b8aeb7dabb4a6b9f0a5e85bd8543b451857ad02c
Nealford Sotes <nsotes@fullscale.io>
2022-11-03 21:23:09 +0800

    initial commit

---
+++ back-end/ContactsApplication/Controllers/ContactsController.cs
@@ -0,0 +1,72 @@
+﻿using ContactsApplication.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly IContactsService _contactsService;
+
+        public ContactsController(IContactsService contactsService)
+        {
+            _contactsService = contactsService;
+        }
+        // GET: api/Contacts
+        [HttpGet]
+        public ActionResult<IEnumerable<Contact>> Get()
+        {
+            var contacts = _contactsService.GetContacts();
+            return Ok(contacts);
+        }
+
+        // GET: api/Contacts/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Contacts
+        [HttpPost]
+        public ActionResult <Contact> CreateContact([FromBody] Contact contact)
+        {
+            _contactsService.AddContact(contact);
+            _contactsService.SaveChanges();
+            return CreatedAtAction(nameof(CreateContact), new { id = contact.Id }, contact);
+        }
+        //DELETE: api/Contacts/{id}
+        [HttpDelete("{id}")]
+        public ActionResult DeleteContact(int id)
+        {
+            var contact = _contactsService.GetContact(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+            _contactsService.DeleteContact(contact);
+            _contactsService.SaveChanges();
+            return NoContent();
+        }
+        // PUT: api/Contacts/{id}
+        [HttpPut("{id}")]
+        public ActionResult UpdateContact(int id, [FromBody] Contact contact)
+        {
+            if (id != contact.Id)
+            {
+                return BadRequest();
+            }
+            var existingContact = _contactsService.GetContact(id);
+            if (existingContact == null)
+            {
+                return NotFound();
+            }
+            _contactsService.UpdateContact(contact);
+            _contactsService.SaveChanges();
+            return NoContent();
+        }
+
+    }
+}
---
+++ back-end/ContactsApplication/Services/JsonFileContactsService.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsApplication.Services
+{
+    public interface IContactsService
+    {
+        IEnumerable<Contact> GetContacts();
+        Contact GetContact(int id);
+        void AddContact(Contact contact);
+        void UpdateContact(Contact contact);
+        void DeleteContact(Contact contact);
+        bool SaveChanges();
+    }
+
+    public class JsonFileContactsService : IContactsService
+    {
+        string fileName;
+        string jsonString;
+        Contact[] contacts;
+        public JsonFileContactsService()
+        {
+             fileName = "contacts.json";
+             jsonString = File.ReadAllText(fileName);
+             contacts = JsonSerializer.Deserialize<Contact[]>(jsonString)!;
+        }
+        
+        public void AddContact(Contact contact)
+        {
+            if (contact == null)
+            {
+                throw new ArgumentNullException(nameof(contact));
+            }
+            foreach (var c in contacts)
+            {
+                if (c.Id == contact.Id)
+                {
+                    throw new ArgumentException("Contact with this id already exists");
+                }
+            }
+            var newContacts = new Contact[contacts.Length + 1];
+            Array.Copy(contacts, newContacts, contacts.Length);
+            newContacts[newContacts.Length - 1] = contact;
+            contacts = newContacts;
+            jsonString = JsonSerializer.Serialize(contacts);
+            File.WriteAllText(fileName, jsonString);
+        }
+
+        public void DeleteContact(Contact contact)
+        {
+            if (contact == null)
+            {
+                throw new ArgumentNullException(nameof(contact));
+            }
+            foreach (var c in contacts)
+            {
+                if (c.Id == contact.Id)
+                {
+                    var newContacts = new Contact[contacts.Length - 1];
+                    var i = 0;
+                    foreach (var c1 in contacts)
+                    {
+                        if (c1.Id != contact.Id)
+                        {
+                            newContacts[i] = c1;
+                            i++;
+                        }
+                    }
+                    contacts = newContacts;
+                    jsonString = JsonSerializer.Serialize(contacts);
+                    File.WriteAllText(fileName, jsonString);
+                    return;
+                }
+            }
+            throw new ArgumentException("Contact with this id does not exist");
+        }
+
+        public Contact GetContact(int id)
+        {
+            return contacts.FirstOrDefault(c => c.Id == id)!;
+        }
+
+        public IEnumerable<Contact> GetContacts()
+        {
+            return contacts.ToList();
+        }
+
+        public bool SaveChanges()
+        {
+            return (contacts.Length > 0);
+        }
+
+        public void UpdateContact(Contact contact)
+        {
+            if (contact == null)
+            {
+                throw new ArgumentNullException(nameof(contact));
+            }
+            foreach (var c in contacts)
+            {
+                if (c.Id == contact.Id)
+                {
+                    c.FirstName = contact.FirstName;
+                    c.LastName = contact.LastName;
+                    c.BillingAddress = contact.BillingAddress;
+                    c.PhysicalAddress = contact.PhysicalAddress;
+                    jsonString = JsonSerializer.Serialize(contacts);
+                    File.WriteAllText(fileName, jsonString);
+                    return;
+                }
+            }
+            throw new ArgumentException("Contact with this id does not exist");
+
+        }
+
+      
+    }
+
+
+
+
+    public class Contact
+    {
+        
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BillingAddress { get; set; }
+        public string PhysicalAddress { get; set; }
+
+       
+    }
+}
